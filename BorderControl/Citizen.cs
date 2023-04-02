c69121df44d471155097d78912ac8d8a0a78ba31
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-04-02 19:08:22 +0300

    Add files via upload

---
+++ BorderControl/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderControl
+{
+    public class Citizen : IId
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+
+        public Citizen(string name, int age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+
+
+    }
+}
---
+++ BorderControl/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            var list = new List<string>();
+
+            while(input[0] != "End")
+            {
+                if(input.Length == 2)
+                {
+                    list.Add(input[1]);
+                }
+                else
+                {
+                    list.Add(input[2]);
+                }
+
+                input = Console.ReadLine().Split();
+            }
+            string finalId = Console.ReadLine();
+            list = list.FindAll(x => x.EndsWith(finalId));
+
+            foreach (var el  in list)
+            {
+                Console.WriteLine(el);
+            }
+            
+
+
+        }
+    }
+}
